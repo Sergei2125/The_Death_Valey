b394ce774bec3310db286fb86d518ceb4402e3f6
Sergei <you@example.com>
2021-10-17 13:58:14 +0300

    corrected the project

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,37 +1,26 @@
 const modal = document.querySelector(".modal");
 const modalImg = document.querySelector(".modal__content");
 
-document.querySelector(".modal").addEventListener("click", () => {
+modal.addEventListener("click", () => {
   modal.classList.remove("showModal");
+  modalImg.setAttribute("src", "");
 });
 
 document
   .querySelector(".work__content")
   .addEventListener("click", function (event) {
     let targetImg = event.target;
-
-    if (targetImg.tagName != "IMG") return;
-
-    modal.classList.add("showModal");
-    modalImg.src = targetImg.src;
+    if (targetImg.tagName !== "IMG") {
+      return;
+    } else {
+      modal.classList.add("showModal");
+      modalImg.setAttribute("src", targetImg.src);
+    }
   });
 
-const formName = document.querySelector(".form__input");
-const formSubject = document.querySelector(".form__subject");
-const formEmail = document.querySelector(".form__email");
-const formMessage = document.querySelector(".form__message");
+const form = document.querySelector(".form");
 
-document.querySelector(".form__submit").addEventListener("click", () => {
-  if (
-    formName.value !== "" &&
-    formSubject.value !== "" &&
-    formEmail.value !== "" &&
-    formMessage.value !== ""
-  ) {
-    formName.value = "";
-    formSubject.value = "";
-    formEmail.value = "";
-    formMessage.value = "";
-    alert("success");
-  }
+document.querySelector(".form").addEventListener("submit", (e) => {
+  form.reset();
+  e.preventDefault();
 });
